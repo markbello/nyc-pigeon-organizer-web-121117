51d9c30095b159a9fadbe6bd7527d92c2675613c
Mark Bello <mcbello51286@gmail.com>
2017-12-06 15:40:40 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,6 +2,8 @@ require "pry"
 def nyc_pigeon_organizer(data)
   # write your code here!
   new_hash = {}
-  data[:color].values.each{|name| if !new_hash[name] new_hash[name] = {}}
+  data[:color].values.each{|name|
+    if !new_hash[name] new_hash[name] = {}
+    }
   binding.pry
 end
