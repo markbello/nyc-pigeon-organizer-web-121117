a5791434935ddf0f0c6b5f8b3a00fc9f5649a25d
Mark Bello <mcbello51286@gmail.com>
2017-12-06 15:41:37 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ require "pry"
 def nyc_pigeon_organizer(data)
   # write your code here!
   new_hash = {}
-  data[:color].values.each{|name|
+  data[:color].values.flatten.each{|name|
     if !new_hash[name]
       new_hash[name] = {}
     end
